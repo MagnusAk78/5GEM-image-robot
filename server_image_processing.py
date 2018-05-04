119bd49b40190f1de90172ffb91585c60a9b0f86
mathisrousseau <38443128+mathisrousseau@users.noreply.github.com>
2018-05-04 10:33:26 +0200

    up and down movement

--- server_image_processing.py
+++ server_image_processing.py
@@ -40,10 +40,14 @@ RAD_TO_DEG_CONV = 57.2958
 PI = math.pi
 MINIMUM_ANGLE_MOVEMENT = PI / 30
 IMAGE_WIDTH = 640
+IMAGE_HEIGHT = 480
 CENTER_X = IMAGE_WIDTH / 2
+CENTER_Y = IMAGE_HEIGHT / 2
 IMAGE_TOTAL_ANGLE = PI / 3
-ANGLE_MIN = PI / 2
-ANGLE_MAX = 5 * PI / 4
+ANGLE_MIN_X = PI / 2
+ANGLE_MAX_X = 5 * PI / 4
+ANGLE_MIN_Y = 0.872 #This limit is equal to 50deg
+ANGLE_MAX_Y = 2.09 #This limit is equal to 120deg
 
 # Logging
 info_logger = helpers.logger.setup_normal_logger('5GEM_robot_demonstrator_info')
@@ -54,9 +58,16 @@ statistics_logger = helpers.logger.setup_normal_logger('5GEM_robot_demonstrator_
 # angle_screen          Total angle in radians over the screen width (float)
 def convert_face_on_screen_to_angle_in_x(face, screen_width, angle_screen, logger):
     xDiff = float(CENTER_X - face.centerX())
-    diffAngle = xDiff * PI / (IMAGE_WIDTH * 8)
-    logger.info('xDiff: ' + str(xDiff) + ', diffAngle (deg): ' + str(diffAngle * RAD_TO_DEG_CONV))
-    return diffAngle
+    diffAngleX = xDiff * PI / (IMAGE_WIDTH * 8)
+    logger.info('xDiff: ' + str(xDiff) + ', diffAngleX (deg): ' + str(diffAngleX * RAD_TO_DEG_CONV))
+    return diffAngleX
+    
+def convert_face_on_screen_to_angle_in_y(face, screen_height, angle_screen, logger):
+    yDiff = float(CENTER_Y - face.centerY())
+    diffAngleY = yDiff * PI / (IMAGE_HEIGHT * 8)
+    logger.info('yDiff: ' + str(yDiff) + ', diffAngleY (deg): ' + \
+                str(diffAngleY * RAD_TO_DEG_CONV) + ' AND diffAngleY (rad): ' + str(diffAngleY))
+    return diffAngleY
     
 class MyRobotConnection():
     
@@ -72,15 +83,18 @@ class MyRobotConnection():
         # UDP VERSION
         #self.imageReader = datagram.image_reader.ImageReader(DATAGRAM_ADDRESS, self.image_queue, info_logger, statistics_logger, LOG_INTERVAL)
         # STREAM VERSION
-        #self.imageReader = mjpeg.mjpeg_stream_reader.MjpegStreamReader(STREAM_URL, READ_CHUNK_SIZE, self.image_queue, info_logger, statistics_logger, LOG_INTERVAL)        
+        #self.imageReader = mjpeg.mjpeg_stream_reader.MjpegStreamReader(STREAM_URL, READ_CHUNK_SIZE, self.image_queue, info_logger, statistics_logger, LOG_INTERVAL)
+        #latency_logger=helpers.LatencyLogger()
         
     def handle_connection(self):
         print('Client connected: ' + self.client_address[0] + ':' + str(self.client_address[1]))
         self.faceDetector.start()
         self.imageReader.start()
         
-        currentRadianValue = float(PI)
-        lastSentValue = currentRadianValue
+        currentRadianValueX = float(PI)
+        currentRadianValueY = 1.571
+        lastSentValueX = currentRadianValueX
+        lastSentValueY = currentRadianValueY
         lastSentTime = time.time()
         faces = 0
         facesSkipped = 0
@@ -101,31 +115,40 @@ class MyRobotConnection():
                         face = self.face_queue.get()
                     
                     if isinstance(face, processing.face.Face):
-                        currentRadianValue = currentRadianValue + convert_face_on_screen_to_angle_in_x(face, IMAGE_WIDTH, IMAGE_TOTAL_ANGLE, info_logger)
+                        currentRadianValueX = currentRadianValueX + convert_face_on_screen_to_angle_in_x(face, IMAGE_WIDTH, IMAGE_TOTAL_ANGLE, info_logger)
+                        currentRadianValueY = currentRadianValueY - convert_face_on_screen_to_angle_in_y(face, IMAGE_HEIGHT, IMAGE_TOTAL_ANGLE, info_logger)
                     else:
                         print("Something is very wrong")
                         break
                         
                     #Make sure the angle is within min/max
-                    if(currentRadianValue < ANGLE_MIN):
-                        currentRadianValue = ANGLE_MIN
-                    if(currentRadianValue > ANGLE_MAX):
-                        currentRadianValue = ANGLE_MAX
+                    if(currentRadianValueX < ANGLE_MIN_X):
+                        currentRadianValueX = ANGLE_MIN_X
+                    if(currentRadianValueX > ANGLE_MAX_X):
+                        currentRadianValueX = ANGLE_MAX_X
+                    #For the axe 4, we change the inequality because we are negative
+                    if(currentRadianValueY < ANGLE_MIN_Y):
+                        currentRadianValueY = ANGLE_MIN_Y
+                    if(currentRadianValueY > ANGLE_MAX_Y):
+                        currentRadianValueY = ANGLE_MAX_Y
                         
-                    if(abs(currentRadianValue - lastSentValue) > MINIMUM_ANGLE_MOVEMENT):
-                        info_logger.info('Sending value = ' + str(currentRadianValue))
-                        self.connection.sendall("(" + str(currentRadianValue) + ",0.300,-0.100,0.300,0,0,0)" + '\n')
+                    if((abs(currentRadianValueX - lastSentValueX) and abs(currentRadianValueY - lastSentValueY)) > MINIMUM_ANGLE_MOVEMENT):
+                        info_logger.info('Sending value for X = ' + str(currentRadianValueX) + ' AND ' + \
+                                        'sending value for Y = ' + str(currentRadianValueY))
+                        self.connection.sendall("(" + str(currentRadianValueX) + "," + str(currentRadianValueY) + ")" + '\n')
                         dataSent = True
                         lastSentTime = now
-                        lastSentValue = currentRadianValue
+                        lastSentValueX = currentRadianValueX
+                        lastSentValueY = currentRadianValueY
                 
                 #If nothing happens we need to send something to keep the connection alive
                 if((now - lastSentTime) > KEEP_ALIVE_INTERVAL):
-                        info_logger.info('Sending value = ' + str(currentRadianValue) + ', *** keep alive ***')
-                        self.connection.sendall("(" + str(currentRadianValue) + ",0.300,-0.100,0.300,0,0,0)" + '\n')
+                        info_logger.info('Sending values = ' + str(currentRadianValueX) + ' AND ' + str(currentRadianValueY) + ', *** keep alive ***')
+                        self.connection.sendall("(" + str(currentRadianValueX) + "," + str(currentRadianValueY) + ")" + '\n')
                         dataSent = True
                         lastSentTime = now
-                        lastSentValue = currentRadianValue
+                        lastSentValueX = currentRadianValueX
+                        lastSentValueY = currentRadianValueY
                 
                 if SHOW_IMAGE_ON_SCREEN and self.show_image_queue.empty() == False:
                     while self.show_image_queue.empty() == False:
